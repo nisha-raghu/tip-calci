8e7d921962f038b1c7230109b01c1f105e69a43e
nisha-raghu <nisha.raghu@sjsu.edu>
2017-03-10 01:11:56 -0800

    Update SettingsViewController.swift

--- Tippy/SettingsViewController.swift
+++ Tippy/SettingsViewController.swift
@@ -2,7 +2,7 @@
 //  SettingsViewController.swift
 //  Tippy
 //
-//  Created by Student on 3/4/17.
+//  Created by Nisha on 3/4/17.
 //  Copyright © 2017 Student. All rights reserved.
 //
 
@@ -10,18 +10,12 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     @IBOutlet weak var tipPercent: UISegmentedControl!
-
-    //@IBOutlet weak var tipPercentControl: UISegmentedControl!
-    
     @IBAction func changeDefaultPercent(_ sender: Any) {
-        print("Change 1")
         let defaults = UserDefaults.standard
         let index = tipPercent.selectedSegmentIndex
         defaults.set(Int(index),forKey: "tip_percent")
         defaults.synchronize()
-
-        
-    }
+     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,15 +33,4 @@ class SettingsViewController: UIViewController {
         tipPercent.selectedSegmentIndex = defaults.integer(forKey: "tip_percent")
         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
